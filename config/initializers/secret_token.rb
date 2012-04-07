4aff5d04883c620e6a9c26ccb8117004876ebf4d
Josh Adams <josh@isotope11.com>
2012-04-07 08:29:45 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpencurriculumFlashcards::Application.config.secret_token = 'd0f4158c0cd81f78f05109c85c53cae5d4feca01d281090b45029a9ac9e2c87a81e5d7888247434bd8d7d1abc83c5098775833171c4501422fa402d2e2eb3166'
